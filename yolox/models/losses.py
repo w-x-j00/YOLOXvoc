e3e97f4548f52b6c02645d34b42fbf0dc5b14154
w-x-j00 <861938307@qq.com>
2022-08-15 20:53:23 +0800

    运行验证voc

--- yolox/models/losses.py
+++ yolox/models/losses.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 # Copyright (c) Megvii Inc. All rights reserved.
-
+import numpy as np
 import torch
 import torch.nn as nn
 
@@ -44,6 +44,37 @@ class IOUloss(nn.Module):
             area_c = torch.prod(c_br - c_tl, 1)
             giou = iou - (area_c - area_u) / area_c.clamp(1e-16)
             loss = 1 - giou.clamp(min=-1.0, max=1.0)
+
+        elif self.loss_type == "siou":
+            c_tl = torch.min(
+                (pred[:, :2] - pred[:, 2:] / 2), (target[:, :2] - target[:, 2:] / 2)
+            )
+            c_br = torch.max(
+                (pred[:, :2] + pred[:, 2:] / 2), (target[:, :2] + target[:, 2:] / 2)
+            )
+            s_cw = (c_br-c_tl)[:, 0]
+            s_ch = (c_br-c_tl)[:, 1]
+            cw = target[:, 0] - pred[:, 0]
+            ch = target[:, 1] - pred[:, 1]
+            sigma = torch.pow(cw ** 2 + ch ** 2, 0.5)
+            sin_alpha = torch.abs(ch) / sigma
+            sin_beta = torch.abs(cw) / sigma
+            thres = torch.pow(torch.tensor(2.), 0.5) / 2
+            sin_alpha = torch.where(sin_alpha < thres, sin_alpha, sin_beta)
+            angle_cost = 1 - 2 * torch.pow(torch.sin(torch.arcsin(sin_alpha) - np.pi/4), 2)
+            gamma = angle_cost - 2
+            rho_x = (cw / s_cw) ** 2
+            rho_y = (ch / s_ch) ** 2
+            distance_cost = 2 - torch.exp(gamma * rho_x) - torch.exp(gamma * rho_y)
+            w_gt = target[:, 2]
+            h_gt = target[:, 3]
+            w_pred = pred[:, 2]
+            h_pred = pred[:, 3]
+            W_w = torch.abs(w_pred - w_gt) / torch.max(w_pred, w_gt)
+            W_h = torch.abs(h_pred - h_gt) / torch.max(h_pred, h_gt)
+            shape_cost = torch.pow(1 - torch.exp(-1 * W_w), 4) + torch.pow(1 - torch.exp(-1 * W_h), 4)
+            siou = iou - (distance_cost + shape_cost) * 0.5
+            loss = 1 - siou.clamp(min=-1.0, max=1.0)
 
         if self.reduction == "mean":
             loss = loss.mean()
